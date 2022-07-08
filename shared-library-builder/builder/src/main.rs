7adefd40c703616b8378b04513fd4d0f6722d828
Aliaksei Syrel <alex.syrel@gmail.com>
2022-07-08 16:13:52 +0430

    add arm64 windows build

--- shared-library-builder/builder/src/main.rs
+++ shared-library-builder/builder/src/main.rs
@@ -1,22 +1,9 @@
-use shared_library_builder::{
-    Library, LibraryCompilationContext, LibraryLocation, LibraryTarget, PathLocation, RustLibrary,
-};
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let library = RustLibrary::new(
-        "Process",
-        LibraryLocation::Path(PathLocation::new(std::env::current_dir().unwrap())),
-    )
-    .package("libprocess");
+use shared_library_builder::build_standalone;
 
-    let context = LibraryCompilationContext::new(
-        "target",
-        "target",
-        LibraryTarget::for_current_platform(),
-        false,
-    );
-    let compiled_library = library.compile(&context)?;
-    println!("Compiled {}", compiled_library.display());
-    Ok(())
+use libprocess_library::latest_libprocess;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    build_standalone(|_| Ok(Box::new(latest_libprocess())))
 }
--- shared-library-builder/library/src/lib.rs
+++ shared-library-builder/library/src/lib.rs
@@ -1,9 +1,14 @@
 use shared_library_builder::{GitLocation, LibraryLocation, RustLibrary};
 
-pub fn libprocess(version: impl Into<String>) -> RustLibrary {
+pub fn libprocess(version: Option<impl Into<String>>) -> RustLibrary {
     RustLibrary::new(
         "Process",
-        LibraryLocation::Git(GitLocation::github("feenkcom", "libprocess").tag(version)),
+        LibraryLocation::Git(GitLocation::github("feenkcom", "libprocess").tag_or_latest(version)),
     )
     .package("libprocess")
+}
+
+pub fn latest_libprocess() -> RustLibrary {
+    let version: Option<String> = None;
+    libprocess(version)
 }
