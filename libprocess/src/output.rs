c6ae0b8b46cd4018dde07579cc73d742cee7ec23
Aliaksei Syrel <alex.syrel@gmail.com>
2024-05-08 17:50:15 +0200

    add lossy string conversion of the output

--- libprocess/src/output.rs
+++ libprocess/src/output.rs
@@ -30,6 +30,18 @@ pub fn process_output_stderr_string(output: *mut ValueBox<Output>) -> *mut Value
 }
 
 #[no_mangle]
+pub fn process_output_stderr_string_lossy(
+    output: *mut ValueBox<Output>,
+) -> *mut ValueBox<StringBox> {
+    output
+        .with_ref_ok(|output| {
+            StringBox::from_string(String::from_utf8_lossy(&output.stderr).to_string())
+        })
+        .map(|stdout| ValueBox::new(stdout))
+        .into_raw()
+}
+
+#[no_mangle]
 pub fn process_output_stdout(output: *mut ValueBox<Output>) -> *mut ValueBox<ArrayBox<u8>> {
     output
         .with_ref_ok(|output| ValueBox::new(ArrayBox::from_vector(output.stdout.clone())))
@@ -44,6 +56,18 @@ pub fn process_output_stdout_string(output: *mut ValueBox<Output>) -> *mut Value
                 .map_err(|error| BoxerError::AnyError(Box::new(error)).into())
         })
         .map(|stdout| ValueBox::new(StringBox::from_string(stdout)))
+        .into_raw()
+}
+
+#[no_mangle]
+pub fn process_output_stdout_string_lossy(
+    output: *mut ValueBox<Output>,
+) -> *mut ValueBox<StringBox> {
+    output
+        .with_ref_ok(|output| {
+            StringBox::from_string(String::from_utf8_lossy(&output.stdout).to_string())
+        })
+        .map(|stdout| ValueBox::new(stdout))
         .into_raw()
 }
 
